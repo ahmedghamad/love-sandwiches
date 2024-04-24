aacd3509d2832de65eb4d85155af151fa1d1e21d
ahmedghamad <manchester-88@hotmail.com>
2024-04-24 15:16:16 +0100

    Added a while loop for the user data request for validation

--- run.py
+++ run.py
@@ -15,15 +15,19 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, seprated by comas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seprated by comas.")
-    print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
 def validate_data(values):
     """
     Inside the try, converts all the string values into intgers.
@@ -38,4 +42,6 @@ def validate_data(values):
                 )
     except ValueError as e:
         print(f"invalid data: {e}, please try agian.\n")
-get_sales_data()
+        return False
+    return True
+data = get_sales_data()
